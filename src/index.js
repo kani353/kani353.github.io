c78861486857ce57399fbb3657966e6e2f571229
kani353 <135725041+kani353@users.noreply.github.com>
2024-10-13 22:40:33 +0900

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const fps = 24;
+const frame = 1000 / fps;
+
+const sheet = "[1]--[2]--[3]--[4]--[5]--[6]--";
+let index = 0;
+
+function drow(){
+    if(index >= sheet.length){
+        index = 0;
+    }
+    const animation = document.getElementById("testImg");
+    const n = sheet.charAt(index);
+    switch(n){
+        case "[":{
+            const brackets = sheet.indexOf("]", index);
+            const count = brackets - index;
+            const cellNumber = sheet.slice(index + 1, index + count);
+
+            const img = cellNumber.padStart(4, "0");
+            const fileName = `img/${img}.png`;
+            animation.src = fileName;
+
+            index += count + 1;
+        }
+        break;
+        case "-":{
+            index ++;
+        }
+    }
+}
+
+setInterval(drow, frame);
